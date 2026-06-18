555766de33271f084b1e1d56836f7448ee4d9163
Ion Socol <157688386+ionis789@users.noreply.github.com>
2026-06-18 13:08:47 +0300

    Match flashcard keyboard clearance

--- QuizFlash/Features/DeckEditor/Components/ZoneEditor/ZoneEditorCanvas.swift
+++ QuizFlash/Features/DeckEditor/Components/ZoneEditor/ZoneEditorCanvas.swift
@@ -1787,7 +1787,7 @@ struct ZoneEditorCanvas: View {
     }
 
     private var caretBottomChromeBuffer: CGFloat {
-        24
+        100
     }
 
     private var dynamicBottomScrollInset: CGFloat {
