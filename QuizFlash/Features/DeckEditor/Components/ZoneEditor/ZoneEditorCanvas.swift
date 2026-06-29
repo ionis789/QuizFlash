e13f8468fe618efeaf9aa336e6b5e737211feba2
Ion Socol <157688386+ionis789@users.noreply.github.com>
2026-06-29 13:48:18 +0300

    Move editor dismiss gesture

--- QuizFlash/Features/DeckEditor/Components/ZoneEditor/ZoneEditorCanvas.swift
+++ QuizFlash/Features/DeckEditor/Components/ZoneEditor/ZoneEditorCanvas.swift
@@ -284,7 +284,6 @@ struct ZoneEditorCanvas: View {
                 .scrollDismissesKeyboard(.never)
                 .scrollClipDisabled(!freezesScrolling)
                 .frame(width: cardWidth, height: editorViewportHeight, alignment: .topLeading)
-                .modifier(ZoneEditorDismissScrollClipModifier(isEnabled: freezesScrolling))
                 .background(windowTouchProbeBackground)
                 .overlay(alignment: .topLeading) {
                     viewportDebugOverlay
@@ -2953,27 +2952,5 @@ struct ZoneEditorCanvas: View {
         AppFeatures.current.showsVisualDebugOverlays
             && showsDebugOverlays
             && developmentPreferences.zoneContentLayoutDebugEnabled
-    }
-}
-
-private struct ZoneEditorDismissScrollClipModifier: ViewModifier {
-    let isEnabled: Bool
-
-    func body(content: Content) -> some View {
-        if isEnabled {
-            content.clipShape(
-                UnevenRoundedRectangle(
-                    cornerRadii: .init(
-                        topLeading: UIConstants.Radius.large,
-                        bottomLeading: 0,
-                        bottomTrailing: 0,
-                        topTrailing: UIConstants.Radius.large
-                    ),
-                    style: .continuous
-                )
-            )
-        } else {
-            content
-        }
     }
 }
