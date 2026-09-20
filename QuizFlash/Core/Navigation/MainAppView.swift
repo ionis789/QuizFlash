4fbe5ac6220d02011ef0d7b4ef6359cb29fb4ce2
Ion Socol <157688386+ionis789@users.noreply.github.com>
2026-09-20 04:36:23 +0300

    Restore tab bar spacing

--- QuizFlash/Core/Navigation/MainAppView.swift
+++ QuizFlash/Core/Navigation/MainAppView.swift
@@ -470,6 +470,10 @@ struct MainAppView: View {
             )
             .animation(.bottomChromeSpring, value: isTabBarCompactedByScroll)
             .offset(y: UIConstants.Layout.bottomChromeVisualBottomOffset)
+            .padding(
+                .bottom,
+                proxy.safeAreaInsets.bottom + UIConstants.Layout.bottomChromeBottomPadding
+            )
             .ignoresSafeArea(.container, edges: isPad ? .bottom : [.horizontal, .bottom])
 
         if usesDetachedPadTabBar {
